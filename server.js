5e94666a0982dbdda570996f2f976a167c58f236
JoyBoyCr7 <Rnyongkah@gmail.com>
2023-08-24 22:19:52 -0500

    CORS in progress

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ const Farmer = mongoose.model('Farmer', farmerSchema)
 app.use(cookieParser())
 app.use(morgan('dev'))
 app.use(cors({
-    origin:'https://u3proapefe.vercel.app',
+    origin:'http://localhost:3000',
     credentials: true,
 }))
 app.use(express.json())
